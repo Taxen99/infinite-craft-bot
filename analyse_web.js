80eb7995b36b3efb68dd881c0bb84041d73bbcab
Taxen99 <maximilian@krig.space>
2024-02-20 10:33:32 +0100

    Update analyse_web.js

--- analyse_web.js
+++ analyse_web.js
@@ -7,7 +7,7 @@ const container_elm = document.getElementById('container');
 const recipe_elm = document.getElementById('recipe');
 input_elm.addEventListener('input', _e => {
   const search_term = input_elm.value;
-  const matches = [...items].filter(x => x.toLowerCase().startsWith(search_term));
+  const matches = [...items].filter(x => x.toLowerCase().includes(search_term.toLowerCase()));
   container_elm.textContent = '';
   recipe_elm.textContent = '';
   for(const match of matches) {
